6ad41f5fa405a7ee71b1e855955d725e24c53e86
Octavian Morcov <octavian.morcov@sap.com>
2019-08-20 21:57:47 +0100

    Connect to cloud mongodb.

--- src/main/java/com/hotel/controllers/RoomController.java
+++ src/main/java/com/hotel/controllers/RoomController.java
@@ -1,14 +1,18 @@
 package com.hotel.controllers;
 
-import com.hotel.dto.*;
-import com.hotel.services.RoomService;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import com.hotel.dto.Room;
+import com.hotel.dto.RoomFacilities;
+import com.hotel.dto.RoomFeature;
+import com.hotel.dto.RoomIdentifier;
+import com.hotel.dto.RoomStatus;
+import com.hotel.dto.RoomType;
+import com.hotel.services.RoomService;
 
 @RestController
 public class RoomController {
@@ -21,13 +25,13 @@ public class RoomController {
     }
 
     @GetMapping(value = "/rooms")
-    public ResponseEntity.BodyBuilder saveRoomToDb(){
+    public Room saveRoomToDb(){
         RoomIdentifier roomIdentifier = new RoomIdentifier("1ab3", 7, 2, 4, RoomType.FAMILY);
         RoomFacilities roomFacilities = new RoomFacilities(Arrays.asList(RoomFeature.BALCONY, RoomFeature.FRIDGE, RoomFeature.TV));
         Room room = new Room(roomIdentifier, 48, "Large family room, up to 4 people.",
                 roomFacilities, RoomStatus.AVAILABLE);
         roomService.addRoom(room);
-        return ResponseEntity.status(HttpStatus.OK);
+        return room;
     }
 
 }
